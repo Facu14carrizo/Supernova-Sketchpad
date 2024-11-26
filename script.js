99de9827c6ea10b33eec00b8cbe6f2ff66d59de5
Facu Carrizo <80929730+Facu14carrizo@users.noreply.github.com>
2024-11-25 21:31:48 -0300

    Add Mobile Screen Support

--- script.js
+++ script.js
@@ -7,84 +7,117 @@ const clearEl = document.getElementById('clear');
 
 const ctx = canvas.getContext('2d');
 
-let size = 10
-let isPressed = false
-colorEl.value = 'black'
-let color = colorEl.value
-let x
-let y
+let size = 10;
+let isPressed = false;
+colorEl.value = 'black';
+let color = colorEl.value;
+let x;
+let y;
 
 canvas.addEventListener('mousedown', (e) => {
-    isPressed = true
+    isPressed = true;
 
-    x = e.offsetX
-    y = e.offsetY
-})
+    x = e.offsetX;
+    y = e.offsetY;
+});
 
-document.addEventListener('mouseup', (e) => {
-    isPressed = false
-
-    x = undefined
-    y = undefined
-})
+document.addEventListener('mouseup', () => {
+    isPressed = false;
+    x = undefined;
+    y = undefined;
+});
 
 canvas.addEventListener('mousemove', (e) => {
-    if(isPressed) {
-        const x2 = e.offsetX
-        const y2 = e.offsetY
+    if (isPressed) {
+        const x2 = e.offsetX;
+        const y2 = e.offsetY;
 
-        drawCircle(x2, y2)
-        drawLine(x, y, x2, y2)
+        drawCircle(x2, y2);
+        drawLine(x, y, x2, y2);
 
-        x = x2
-        y = y2
+        x = x2;
+        y = y2;
     }
-})
+});
+
+// Eventos para dispositivos móviles
+canvas.addEventListener('touchstart', (e) => {
+    isPressed = true;
+
+    // Prevenir el comportamiento predeterminado del navegador
+    e.preventDefault();
+
+    // Obtener la posición del toque
+    const touch = e.touches[0];
+    x = touch.offsetX;
+    y = touch.offsetY;
+});
+
+canvas.addEventListener('touchend', () => {
+    isPressed = false;
+    x = undefined;
+    y = undefined;
+});
+
+canvas.addEventListener('touchmove', (e) => {
+    if (isPressed) {
+        e.preventDefault(); // Prevenir el desplazamiento de la página
+
+        const touch = e.touches[0];
+        const x2 = touch.offsetX;
+        const y2 = touch.offsetY;
+
+        drawCircle(x2, y2);
+        drawLine(x, y, x2, y2);
+
+        x = x2;
+        y = y2;
+    }
+});
 
 function drawCircle(x, y) {
     ctx.beginPath();
-    ctx.arc(x, y, size, 0, Math.PI * 2)
-    ctx.fillStyle = color
-    ctx.fill()
+    ctx.arc(x, y, size, 0, Math.PI * 2);
+    ctx.fillStyle = color;
+    ctx.fill();
 }
 
 function drawLine(x1, y1, x2, y2) {
-    ctx.beginPath()
-    ctx.moveTo(x1, y1)
-    ctx.lineTo(x2, y2)
-    ctx.strokeStyle = color
-    ctx.lineWidth = size * 2
-    ctx.stroke()
+    ctx.beginPath();
+    ctx.moveTo(x1, y1);
+    ctx.lineTo(x2, y2);
+    ctx.strokeStyle = color;
+    ctx.lineWidth = size * 2;
+    ctx.stroke();
 }
 
 function updateSizeOnScreen() {
-    sizeEL.innerText = size
+    sizeEL.innerText = size;
 }
 
 increaseBtn.addEventListener('click', () => {
-    size += 5
+    size += 5;
 
-    if(size > 50) {
-        size = 50
+    if (size > 50) {
+        size = 50;
     }
 
-    updateSizeOnScreen()
-})
+    updateSizeOnScreen();
+});
 
 decreaseBtn.addEventListener('click', () => {
-    size -= 5
+    size -= 5;
 
-    if(size < 5) {
-        size = 5
+    if (size < 5) {
+        size = 5;
     }
 
-    updateSizeOnScreen()
-})
+    updateSizeOnScreen();
+});
 
-colorEl.addEventListener('change', (e) => color = e.target.value)
+colorEl.addEventListener('change', (e) => color = e.target.value);
 
-clearEl.addEventListener('click', () => ctx.clearRect(0,0, canvas.width, canvas.height))
-
+clearEl.addEventListener('click', () => ctx.clearRect(0, 0, canvas.width, canvas.height));
 
 function createStars(numStars) {
     const body = document.body;
@@ -94,9 +127,9 @@ function createStars(numStars) {
         star.classList.add('star');
 
         // Establecer tamaño y posición aleatoria de estrellas
-        const size = Math.random() * 3 + 1; 
-        star.style.width = `${size}px`;
-        star.style.height = `${size}px`;
+        const sizeStar = Math.random() * 3 + 1; 
+        star.style.width = `${sizeStar}px`;
+        star.style.height = `${sizeStar}px`;
         star.style.top = `${Math.random() * 100}vh`; 
         star.style.left = `${Math.random() * 100}vw`; 
 
